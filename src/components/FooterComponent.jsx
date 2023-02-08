86c6eab63331d3a09ef36ccd991f91e202dc1b39
SalvatoreCodes <100625922+SalvatoreCodes@users.noreply.github.com>
2023-02-08 08:53:23 +0800

    all rights reserved
    sheesh

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -32,6 +32,7 @@ function FooterComponent() {
       <div className='map'>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d249.2959494202826!2d124.83772645454982!3d1.3350917478953186!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2f84957ec5fe9d3d!2sChristian%20Vocational%20School%202%20-%20Tomohon!5e0!3m2!1sen!2sid!4v1675226506689!5m2!1sen!2sid" style={{border: '0'}} className='map-content' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
+      <p>Copyright @2023 All rights reserved | This website was made by Putra Goni (PPLG)</p>
     </footer>
   )
 }
