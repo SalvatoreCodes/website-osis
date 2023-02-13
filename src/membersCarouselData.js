654a9cb2c7cc1cf1ddfd1c8a1afd79e5a0d87a66
SalvatoreCodes <100625922+SalvatoreCodes@users.noreply.github.com>
2023-02-13 11:24:21 +0800

    fix?
    bruh

--- src/membersCarouselData.js
+++ src/membersCarouselData.js
@@ -1,27 +1,8 @@
-import osis55 from "./members/membersCarousel/osis55.jpg";
-import DSC_0231 from "./members/membersCarousel/DSC_0231.jpg";
-import DSC_0227 from "./members/membersCarousel/DSC_0227.jpg";
-import DSC_0225 from "./members/membersCarousel/DSC_0225.jpg";
+import agnes from "./members/agnes.jpg";
 
 export const membersCarouselData = [
   {
     id: 1,
-    image: osis55,
-  },
-  {
-    id: 2,
-    image: osis55,
-  },
-  {
-    id: 3,
-    image: DSC_0231,
-  },
-  {
-    id: 4,
-    image: DSC_0227,
-  },
-  {
-    id: 5,
-    image: DSC_0225,
+    image: agnes,
   },
 ];
