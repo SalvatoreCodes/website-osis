98afa47bb5f8a26c0f2e765f65eb119172175518
SalvatoreCodes <100625922+SalvatoreCodes@users.noreply.github.com>
2023-02-13 11:30:28 +0800

    carousel

--- src/membersCarouselData.js
+++ src/membersCarouselData.js
@@ -1,8 +1,23 @@
-import agnes from "./members/agnes.jpg";
+import Carousel1 from "./members/membersCarousel/carousel1.jpg";
+import Carousel2 from "./members/membersCarousel/carousel2.jpg";
+import Carousel3 from "./members/membersCarousel/carousel3.jpg";
+import Carousel4 from "./members/membersCarousel/carousel4.jpg";
 
 export const membersCarouselData = [
   {
     id: 1,
-    image: agnes,
+    image: Carousel1,
+  },
+  {
+    id: 2,
+    image: Carousel2,
+  },
+  {
+    id: 3,
+    image: Carousel3,
+  },
+  {
+    id: 4,
+    image: Carousel4,
   },
 ];
